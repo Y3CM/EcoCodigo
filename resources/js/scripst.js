71772aa30a7baa309d4818517fd030f17c0b6e0d
Y3CM <pauljons27@gmail.com>
2025-11-29 18:53:41 -0500

    feat:implementar sistema de registro e inicio de sesion

--- resources/js/scripst.js
+++ resources/js/scripst.js
@@ -1,4 +1,16 @@
 console.log("EcoCodigo");
+ 
+let login = localStorage.getItem("login");
+
+  if (login != undefined) {
+    let users = localStorage.getItem("users");
+    users = JSON.parse(users);
+    users.forEach((item) => {
+      saludo.innerHTML = `Bienvenido ${item.nombre} ${item.apellido}`;
+    });
+  } else {
+    window.location.href = "login.html";
+  }
 
 const canva = document.getElementById("mycanvas");
 const ctx = canva.getContext("2d");
